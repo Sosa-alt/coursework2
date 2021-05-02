e81e9ece1af041d40c68264f20f40d595f83ee5b
Freeman-md <freemanmadudili@gmail.com>
2021-05-02 18:26:58 +0100

    Static File Middleware Implementation
    Middleware that returns lesson images or an error message if the image file does not exist

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const app = express();
 // Use logger middleware
 app.use(require('./logger'));
 
+// Static file middleware
+app.use(express.static('public'));
+
 // Listen for requests
 app.listen(process.env.PORT || PORT, () => {
     console.log('Now listening for requests.');
