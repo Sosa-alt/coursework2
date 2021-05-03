ee91b296f01c5eb74890707520ac65e5cc8db719
Freeman-md <freemanmadudili@gmail.com>
2021-05-03 20:00:57 +0100

    Refactored Code 3

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header({
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json'
+    });
     next();
 });
 
