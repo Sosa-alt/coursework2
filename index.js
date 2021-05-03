b289261b2b385df69e35eb820994519abf6f589c
Freeman-md <freemanmadudili@gmail.com>
2021-05-03 20:48:50 +0100

    Refactored Code 3

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 4000;
 app.use((req, res, next) => {
     res.header({
         'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': [POST, PUT, GET, OPTIONS],
+        'Access-Control-Allow-Methods': 'POST, PUT, GET, OPTIONS',
         'Content-Type': 'application/json'
     });
     next();
